55cbeddd117ef7af6ade428a8cb8446fd9dc167d
Michelle Rodriguez Harrison <dev@michellerh.com>
2019-12-13 22:54:00 -0500

    Tried and skipped array section before breaking my keyboard. Completed Higher Order functions. Working on closure explanation

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -55,6 +55,13 @@ console.log(dino1.period);
 console.log(dino1.speak());
 
 
+
+
+
+
+
+
+
 // ==== Arrays ====
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
@@ -78,7 +85,11 @@ Once you have the new array created, sort the universities alphabetically and lo
 const universities = [];
 console.log(universities);
 
+
+
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+
+
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
@@ -86,6 +97,9 @@ The resulting contact information strings should have a space between the first 
 Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
+
+
+
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
